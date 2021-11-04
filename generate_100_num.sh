3580df0fc84551cd1f6fd640a9c52c07484f6582
Hassan Dufer <hadufer@c1r2p7.42nice.fr>
2021-11-04 16:53:59 +0100

    Push_swap working 5/5 on 100 3/5 500

--- generate_100_num.sh
+++ generate_100_num.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-ECHO=$(echo -n)
-# for i in {0..99}
-# do
-# 	printf '\"%d\", ' $(echo "$RANDOM % 10000"| bc)
-# done
-
-for i in {0..499}
+TMP=0
+for i in {0..99}
 do
-	printf '%d ' $(echo "$RANDOM % 10000"| bc)
+	TMP=$(expr $(./push_swap `ruby -e "puts (0..500).to_a.shuffle.join(' ')"` | wc -w | bc) + $TMP)
 done
+TMP=$(expr $TMP / 100)
+echo $TMP
--- logic.c
+++ logic.c
@@ -6,7 +6,7 @@
 /*   By: hadufer <hadufer@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 13:40:22 by hadufer           #+#    #+#             */
-/*   Updated: 2021/11/03 17:37:53 by hadufer          ###   ########.fr       */
+/*   Updated: 2021/11/04 16:53:11 by hadufer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,11 @@ int	compute_quarter_size(t_list *a)
 
 	list_size = list_get_size(a);
 	if (list_size < 90)
-		return (4);
+		return (1);
 	else if (list_size >= 90 && list_size <= 110)
 		return (4);
 	else
 		return (8);
-	return (1);
 }
 
 int	logic(t_list **a, t_list **b, t_list **op_list)
@@ -45,19 +44,23 @@ int	logic(t_list **a, t_list **b, t_list **op_list)
 				acc += list_get_size(*a) / quarter_size + 1;
 		else if (list_get_index(*a, get_unit_quarter(*a, acc)->number) > 0)
 			median_rotate(a, get_unit_quarter(*a, acc)->number, op_list, 'a');
-		// else if (list_get_index(*a, get_min_unit(*a)->number) > 0)
-		// 	median_rotate(a, get_min_unit(*a)->number, op_list, 'a');
 		else
-			push(a, b, op_list, ft_strdup("pb"));
+				push(a, b, op_list, ft_strdup("pb"));
+	}
+	// problem du ko 96 -97 patch ici
+	if (list_get_size(*a) > 3 && !is_top_3_successor(a))
+	{
+		while (list_get_index(*a, get_max_unit(*b)->number) > 0)
+			median_rotate(a, get_max_unit(*a)->number, op_list, 'a');
+		push(a, b, op_list, ft_strdup("pb"));
 	}
 	// SORT LE DERNIER QUARTER SANS PUSH
 	while (list_get_size(*b))
 	{
-		// if (list_get_size(*b) >= 3 && is_top_3_successor(b))
-		// 	handler_case_3(b, op_list, 'b');
 		while (list_get_index(*b, get_max_unit(*b)->number) > 0)
 			median_rotate(b, get_max_unit(*b)->number, op_list, 'b');
 		push(b, a, op_list, ft_strdup("pa"));
+		// print_debug(a, b);
 	}
 	// print_debug(a, b);
 	print_operation(op_list);
